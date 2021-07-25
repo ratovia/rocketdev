a2adc8d3ad539b1d39537b04a166bc6a240625b7
ratovia <ratovia@hotmail.co.jp>
2021-07-25 09:48:28 +0900

    github view

---
+++ src/components/webview/githubWebview.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  webview: {
+    padding: 0,
+    flexGrow: 1,
+  },
+}));
+
+interface Props {
+  url: string;
+}
+
+const GitbubWebview = ({ url }: Props) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.webview}>
+      <webview
+        src={url}
+        style={{
+          display: 'inline-flex',
+          width: '100%',
+          height: '100%',
+        }}
+      />
+    </div>
+  );
+};
+
+export default GitbubWebview;
